852825d883991bbe48872dc58aae0caee9c20854
t3zeng <t3zeng@uwaterloo.ca>
2014-10-14 19:25:47 -0400

    Made more progress.

--- RTOS Project 2/t3zeng_apasquar_ Project2.c
+++ RTOS Project 2/t3zeng_apasquar_ Project2.c
@@ -25,8 +25,8 @@ int bucket[10] = {NULL};
 int main(void)
 {
 	half_init();
-	half_alloc( 81 );
-	half_alloc( 1325 );
+	half_alloc( 8100 );
+	half_alloc( 13215 );
 	printf("\n\nHeader of the first block:\n");
 	printf("%d ", get_prev(0));
 	printf("%d ", get_next(0));
@@ -47,64 +47,64 @@ int main(void)
 }
 //Series of get functions to retrieve values from the block of memory
 
-U16 get_prev(int index)
+U32 get_prev(int index)
 {
-	return  my_mem[index] >> 22;
+	return  (my_mem[index] >> 22) << 3;
 }
-U16 get_next(int index)
+U32 get_next(int index)
 {
-	return my_mem[index] << 10 >> 22;
+	return (my_mem[index] << 10 >> 22) <<3;
 }
-U16 get_block_size(int index)
+U32 get_block_size(int index)
 {
 	if(my_mem[index] << 20 >> 22 == 0)
 		return 1024;
 	return my_mem[index] << 20 >> 22;
 }
-U16 get_flag(int index)
+U32 get_flag(int index)
 {
 	return my_mem[index] << 30 >> 31;
 }
-U16 get_prev_bucket(int index)
+U32 get_prev_bucket(int index)
 {
-	return my_mem[index+1] >> 22;
+	return (my_mem[index+1] >> 22) << 3;
 }
-U16 get_next_bucket(int index)
+U32 get_next_bucket(int index)
 {
-	return my_mem[index+1] << 10 >> 22;
+	return (my_mem[index+1] << 10 >> 22) << 3;
 }
 
 //Series of set functions to store values to the block of memory
-//Takes the index and a U16 value
-void set_prev(int index, U16 prev)
+//Takes the index and a U32 value
+void set_prev(int index, U32 prev)
 {
 	my_mem[index] = my_mem[index] & 0x3FFFFF;
-	my_mem[index] += prev << 22;
+	my_mem[index] += prev >> 3 << 22 ;
 }
-void set_next(int index, U16 next)
+void set_next(int index, U32 next)
 {
 	my_mem[index] = my_mem[index] & 0xFFC00FFF;
-	my_mem[index] += next << 12;
+	my_mem[index] += next >> 3 << 12;
 }
-void set_block_size(int index, U16 size)
+void set_block_size(int index, U32 size)
 {
 	my_mem[index] = my_mem[index] & 0xFFFFF003;
 	my_mem[index] += size << 2;
 }
-void set_flag(int index, U16 flag)
+void set_flag(int index, U32 flag)
 {
 	my_mem[index] = my_mem[index] & 0xFFFFFFFD;
 	my_mem[index] += flag << 1;
 }
-void set_prev_bucket(int index, U16 prev_bucket)
+void set_prev_bucket(int index, U32 prev_bucket)
 {
 	my_mem[index+1] = my_mem[index+1] & 0x3FFFFF;
-	my_mem[index+1] += prev_bucket << 22;
+	my_mem[index+1] += prev_bucket >> 3 << 22;
 }
-void set_next_bucket(int index, U16 next_bucket)
+void set_next_bucket(int index, U32 next_bucket)
 {
 	my_mem[index+1] = my_mem[index+1] & 0xFFC00FFF;
-	my_mem[index+1] += next_bucket << 12;
+	my_mem[index+1] += next_bucket >> 3 << 12;
 }
 
 //initialize a system with one bin of 32768 bytes of free memory with corresponding pointers
@@ -141,9 +141,11 @@ void *half_alloc( int n )
 		printf("ERROR:INVALID AMOUNT REQUESTED\n");
 	else
 	{
+		//moves the number up to the nearest 32 as memory can only be stored in 32s
+		n += n%32;
 		//divides n by 32 to quickly place things in a bucket
 		int counter = 0;
-		while((n > (16*(2<<counter)) && counter < 10) || (bucket[counter] == NULL && counter < 10))
+		while((n > (16*(2<<counter)) && counter < 10) || (bucket[counter] == NULL && counter < 10)) // potential for error check after getting things in multiple buckets
 			counter++;
 		//printf("%d\n", counter);
 		//No space is available for allocation of a block of that size
@@ -157,7 +159,6 @@ void *half_alloc( int n )
 				//the size is changed to reflect the new smaller size of the free mem
 				//Difference of bucket and &my_mem[0] allows us to get the index in the array to store the new headers
 				int index = bucket[counter];
-				printf("%d\n", index);
 
 				//sets each part of the header 1 index from each other starting at index+n+4 which is the end of the now allocated n bytes of memory
 				set_prev(index+(int)ceil(n/4.0), index);
@@ -166,7 +167,6 @@ void *half_alloc( int n )
 					set_next(index+(int)ceil(n/4.0), index+(int)ceil(n/4.0));
 				else
 					set_next(index+(int)ceil(n/4.0), get_next(index));
-
 				set_block_size(index+(int)ceil(n/4.0), get_block_size(index)-(int)ceil(n/32.0));
 				set_flag(index+(int)ceil(n/4.0), 0);
 				set_prev_bucket(index+(int)ceil(n/4.0), index+(int)ceil(n/4.0));
@@ -174,18 +174,17 @@ void *half_alloc( int n )
 				if(get_next_bucket(index) == index)
 					set_next_bucket(index+(int)ceil(n/4.0), index+(int)ceil(n/4.0));
 				set_next_bucket(index+(int)ceil(n/4.0), get_next_bucket(index));
-				printf("%d\n", (8*(2<<counter) + 4)/32);
 
+				printf("%d\n", get_block_size(index+(int)ceil(n/4.0)));
 				//Checks if the remaining memory after allocation is enough to form a new block of free memory with a header and creates them
-				if(get_block_size(index)-(int)ceil(n/32.0) > (8*(2<<counter))/32 + 4)
+				if(get_block_size(index+(int)ceil(n/4.0))-(int)ceil(n/32.0) >= (8*(2<<counter))/32 + 4)
 				{
 					printf("1\n");
 					//change bucket[counter]'s address to newly located header
 					bucket[counter] += (int)ceil(n/4.0);
 				}
 				//handles the case where the new the memory must be placed in a smaller bucket
-				else if(get_block_size(index+(int)ceil(n/4.0)) - (int)ceil(n/32.0)
-						< 16*(2<<counter) + 4)
+				else if(get_block_size(index+(int)ceil(n/4.0)) - (int)ceil(n/32.0) < (8*(2<<counter))/32 + 4)
 				{
 					printf("2\n");
 					int temp = bucket[counter];
@@ -213,7 +212,6 @@ void *half_alloc( int n )
 				set_block_size(index, (int)ceil(n/32.0));
 				set_flag(index, 1);
 				set_next(index, index+(int)ceil(n/4.0));
-				printf("complete\n");
 			}
 		}
 	}
